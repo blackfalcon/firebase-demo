748e91add40e84cfe38487e38cbc234d8f20b828
Dale Sande <dsande@spiria.com>
2018-06-19 15:48:39 -0700

    adds config console.log to validate it's working

--- src/initializeApp.js
+++ src/initializeApp.js
@@ -5,3 +5,6 @@ firebase.initializeApp({
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_DATABASE_URL,
 });
+
+// Validate that .env variables are seen in the app
+// console.log(process.env.REACT_APP_API_KEY)
